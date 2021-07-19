27910ac4bc3010d756dc6d0bab92410bb450dd10
Thambolo <84055721+Thambolo@users.noreply.github.com>
2021-07-19 16:42:47 +0800

    Add files via upload

---
+++ ageGenderInsert.py
@@ -0,0 +1,35 @@
+import mysql.connector as mysql
+import random
+from datetime import datetime
+import requests #added
+db = mysql.connect(
+port = 3307,
+host = "localhost",
+user = "root",
+passwd = "",
+database = "ep0403"
+)
+## tweeter
+tweetApiKey = "Z57OA2M6UVW36FGR" #add our own key
+
+## get ML data age and gender
+age = random.randrange(16,50)
+genderList = ['male','female']
+gender = random.choice(genderList)
+
+now = datetime.now()
+current_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+requests.post('https://api.thingspeak.com/apps/thingtweet/1/statuses/update',
+            json={'api_key':tweetApiKey,'status':'ALERT: Possible Intruder!\nTime: {time}\nGender: {g}\nAge: {a}'.format(time=current_time,g=gender,a=age)})
+
+cursor = db.cursor()
+## defining the Query
+query = "INSERT INTO predictions (age, gender) VALUES (%s, %s)"
+## storing values in a variable
+values = (age, gender)
+## executing the query with values
+cursor.execute(query, values)
+## to make final output we have to run the 'commit()' method of the database object
+db.commit()
+print(cursor.rowcount, "row, Gender:{gender} Age:{age} inserted".format(gender=gender,age=age))
+
---
+++ ep0403.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 19, 2021 at 10:42 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ep0403`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passwd`
+--
+
+CREATE TABLE `passwd` (
+  `id` int(11) NOT NULL,
+  `pin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `predictions`
+--
+
+CREATE TABLE `predictions` (
+  `id` int(11) NOT NULL,
+  `gender` tinytext NOT NULL,
+  `age` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `predictions`
+--
+
+INSERT INTO `predictions` (`id`, `gender`, `age`) VALUES
+(1, 'female', '38'),
+(2, 'female', '26'),
+(3, 'female', '28'),
+(4, 'male', '31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `passwd`
+--
+ALTER TABLE `passwd`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `predictions`
+--
+ALTER TABLE `predictions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `passwd`
+--
+ALTER TABLE `passwd`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `predictions`
+--
+ALTER TABLE `predictions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
