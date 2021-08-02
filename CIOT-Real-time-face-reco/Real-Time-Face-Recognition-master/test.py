65bab26389632ed826cbc02c491927ff496dd8a7
Zetares <81829035+Zetares@users.noreply.github.com>
2021-08-02 16:15:13 +0800

    change the change

--- CIOT-Real-time-face-reco/Real-Time-Face-Recognition-master/test.py
+++ CIOT-Real-time-face-reco/Real-Time-Face-Recognition-master/test.py
@@ -1,46 +1,54 @@
 import mysql.connector as mysql
+import datetime
+from datetime import date
+timenow = datetime.datetime.now()
 
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "",
-    database = "ep0403",
-    port = 3307
-)
+dt_string = timenow.strftime("%d-%m-%Y_%H:%M:%S")
+print(dt_string)
+today = date.today()
+print(today)
 
-cursor = db.cursor()
-passwordlist = []
+# db = mysql.connect(
+#     host = "localhost",
+#     user = "root",
+#     passwd = "1qwer$#@!",
+#     database = "ep0403",
+#     port = 3306
+# )
+
+# cursor = db.cursor()
+# passwordlist = []
 
 
-## getting all the tables which are present in ‘ep0403’ database
-query = "SELECT pin FROM passwd"
+# ## getting all the tables which are present in ‘ep0403’ database
+# query = "SELECT pin FROM passwd"
 
-cursor.execute(query)
+# cursor.execute(query)
 
-allpasswords = cursor.fetchall() ## it returns list of tables present in the
+# allpasswords = cursor.fetchall() ## it returns list of tables present in the
 
-length = len(allpasswords)
+# length = len(allpasswords)
 
-for password in allpasswords:
-    for passwd in password:
-        passwordlist.append(passwd)
+# for password in allpasswords:
+#     for passwd in password:
+#         passwordlist.append(passwd)
 
-failcount = 0
+# failcount = 0
 
-while failcount < 2:
-    guess = int(input("password? : "))
-    for i in passwordlist:
-        if guess == i:
-            failcount = 3
-    failcount += 1
+# while failcount < 2:
+#     guess = int(input("password? : "))
+#     for i in passwordlist:
+#         if guess == i:
+#             failcount = 3
+#     failcount += 1
 
-print(failcount)
+# print(failcount)
 
-if failcount == 2:
-    print("password wrong")
-else:
-    print("password correct")
-print("end")
+# if failcount == 2:
+#     print("password wrong")
+# else:
+#     print("password correct")
+# print("end")
 
 
 
